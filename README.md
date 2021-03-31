3d185fe967f124eb6140c339c65adcb26d880449
WinAuD <mailchris1@gmx.de>
2021-03-31 14:46:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# lab02-gevjz8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/lab02-gevjz8)
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import './App.css';
+ 
+class App extends Component {
+  render() {
+    return (
+      <div className="page">
+        <h3>Hello, my world!</h3>
+      </div>
+    );
+  }
+}
+export default App;    
